74eae6e0699b425ee7bebe2820798886e84f5e9d
Gleyson Abreu <gleyson_datu@hotmail.com>
2023-07-26 18:27:18 -0300

    feat: add button variants purple-pink and gray

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,17 +1,22 @@
 'use client';
 import { CircleNotch } from "@phosphor-icons/react";
+import clsx from "clsx";
 import { ButtonHTMLAttributes, ReactNode } from "react";
 
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
   children: ReactNode;
   isLoading?: boolean;
+  variant?: "purple-pink" | "gray";
 }
 
-export function Button({ children, isLoading,...rest }: ButtonProps) {
+export function Button({ children, isLoading, variant = 'purple-pink',...rest }: ButtonProps) {
   return (
     <button
     {...rest}
-    className="flex items-center justify-center px-4 py-2 gap-2 rounded-lg transition-all bg-gradient-to-r from-brand-purple to-brand-pink hover:from-brand-purple-light hover:to-brand-pink-light disabled:cursor-not-allowed disabled:from-brand-purple-light disabled:to-brand-pink-light">
+    className={clsx("flex items-center justify-center px-4 py-2 gap-2 rounded-lg disabled:cursor-not-allowed transition-all ", {
+      'bg-gradient-to-r text-white from-brand-purple to-brand-pink hover:from-brand-purple-light hover:to-brand-pink-light disabled:from-brand-purple-light disabled:to-brand-pink-light': variant === 'purple-pink',
+      'bg-brand-gray text-white hover:bg-brand-gray-medium disabled:bg-brand-gray-medium': variant === 'gray',
+    })}>
       {isLoading ? (
         <div className="flex items-center justify-center w-8 h-8 rounded-full shrink-0 bg-white bg-opacity-20">
           <CircleNotch size={18} className="animate-spin" />
