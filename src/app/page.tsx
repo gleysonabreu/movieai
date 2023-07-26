ea54ed5829145864f171f8115c53d1e665cc2039
Gleyson Abreu <gleyson_datu@hotmail.com>
2023-07-26 18:09:44 -0300

    feat: create header and main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,23 @@
+'use client';
+import { Lightning } from "@phosphor-icons/react";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <h1>hello world.</h1>
+    <main className="flex py-16 px-24 flex-col justify-center items-center gap-9 min-h-screen">
+      <header className="w-full flex items-center justify-between">
+        <Link href='/'>
+          <Image src='/logo.svg' alt='Logo' width={85} height={44} />
+        </Link>
+
+        <button className="flex items-center justify-center px-4 py-2 gap-2 rounded-lg transition-all bg-gradient-to-r from-brand-purple to-brand-pink hover:from-brand-purple-light hover:to-brand-pink-light">
+          Nova recomendação
+          <div className="flex items-center justify-center w-8 h-8 rounded-full shrink-0 bg-white bg-opacity-20">
+            <Lightning size={18} />
+          </div>
+        </button>
+      </header>
+    </main>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,19 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        brand: {
+          purple: '#8323FF',
+          'purple-light': '#9F55FF',
+          pink: '#FF2DAF',
+          'pink-light': '#FF46B9',
+          yellow: '#FEEA35',
+          'gray-dark': '#1E1F28',
+          gray: '#2B2B37',
+          'gray-medium': '#373745',
+          'gray-light': '#8B8D9B'
+        }
+      }
     },
   },
   plugins: [],
